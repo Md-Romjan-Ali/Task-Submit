601fd4e3227d85cc9f58d0f22b6524ed4f61fa7b
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-08-17 21:35:56 +0800

    update getTask function

--- src/lib/get.ts
+++ src/lib/get.ts
@@ -1,10 +1,12 @@
 const serverUri = process.env.NEXT_PUBLIC_SERVER_URI
-
-export const getTask = async () => {
-    const res = await fetch(`${serverUri}/api/getsubmit`, {
+const fetchData = async (endPoint: string) => {
+    const res = await fetch(`${serverUri}${endPoint}`, {
         cache: 'no-store'
     })
     return res.json()
+}
+export const getTask = async () => {
+    return fetchData(`/api/getsubmit`)
 }
 export const getDataByEmail = async (email?: string) => {
     const res = await fetch(`${serverUri}/api/getsubmitbyemail?email=${email}`, {
